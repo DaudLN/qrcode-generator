46855d7d6091dcfdb62e1d7d6724ee6ce31620c2
Daud Namayala <83674818+DaudLN@users.noreply.github.com>
2023-02-06 03:41:39 +0300

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("events/", views.render_events, name="events"),
     path("", views.render_qrcode_template, name="qrcode"),
-    path("qrgen/", views.generate_bar_code, name="qrcode-generator")
-
+    path("events/", views.render_events, name="events"),
+    path("qrgen/", views.generate_bar_code, name="qrcode-generator"),
 ]
